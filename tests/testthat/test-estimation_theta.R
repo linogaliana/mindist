3472b66954345525b344348206693f370d0d518a
Lino Galiana <linogaliana@yahoo.fr>
2020-12-31 15:35:17 +0100

    additional test

--- tests/testthat/test-estimation_theta.R
+++ tests/testthat/test-estimation_theta.R
@@ -10,6 +10,7 @@ sd <- 2
 
 # PART 1: ESTIMATE PARAMETERS FROM NORMAL DISTRIBUTION --------------
 
+## EXPERIMENTATION 1
 
 x <- rnorm(n, mu, sd)
 theta_observed <- c(mean(x),sd(x))
@@ -24,20 +25,13 @@ moment_function <- function(theta, ...){
   )
 }
 
-objective_function <- function(theta, prediction_function, weights = 1L, return_moment = FALSE, ...){
-  if (return_moment) return(moment_function(theta))
-  if (length(weights)==1) return( t(moment_function(theta, ...)$epsilon) %*% moment_function(theta, ...)$epsilon)
-  eps <- moment_function(theta, ...)$epsilon
-  return(t(eps) %*% weights %*% eps)
-}
-
 msm1 <- estimation_theta(theta_0 = c("mu" = 0, "sigma" = 0.2),
-                         model_function = objective_function,
+                         # model_function = objective_function,
                          prediction_function = moment_function,
                          approach = "one_step")
 
 msm2 <- estimation_theta(theta_0 = c("mu" = 0, "sigma" = 0.2),
-                         model_function = objective_function,
+                         # model_function = objective_function,
                          prediction_function = moment_function,
                          approach = "two_step")
 
@@ -50,35 +44,61 @@ test_that("Method of simulated moments should be close from theoretical paramete
   expect_equal(c(mu,sd), as.numeric(msm2$NelderMead$`NM_step1`$`par`), tolerance = 10e-1)
 }))
 
+## EXPERIMENTATION 2
+
+x <- 0.1 + 0.2*rnorm(1000L)
+theta_observed <- c(mean(x), sqrt(mean((x - mean(x))^2)))
+
+moment_function <- function(theta, ...){
+  x_hat <- rnorm(n, mean = theta[1], sd = theta[2])
+  theta_simul <- c(mean(x_hat), sqrt(mean((x - mean(x_hat))^2)))
+  return(
+    data.table::data.table(
+      'epsilon' = theta_observed - theta_simul
+    )
+  )
+}
+
+
+msm2 <- estimation_theta(theta_0 = c("mu" = 0, "sigma" = 0.2),
+                         prediction_function = moment_function,
+                         approach = "two_step")
+
+
+test_that("Method of simulated moments should be close from theoretical parameters", ({
+  expect_equal(c(0.1, 0.2), as.numeric(msm2$NelderMead$`NM_step1`$`par`), tolerance = 10e-1)
+}))
 
 
 # PART 2: REPLICATE OLS --------------------------------
 
 
-# x <- replicate(ncol, rnorm(n))
-#
-# df <- data.frame(x1 = x[,1], x2 = x[,2],
-#                  x3 = x[,3])
-#
-# df$y <- 1 + 2*df$x1 + rnorm(n)
-#
-#
-# prediction_function <- function(theta){
-#   return(
-#     cbind(1L, df$x1) %*% theta
-#   )
-# }
-#
-# # FORMALISM REQUIRED FOR OUR FUNCTIONS
-# moment_function <- function(theta){
-#   return(
-#     data.table::data.table(
-#       'epsilon' = as.numeric(df$x1*(df$y - cbind(1L, df$x1) %*% theta))
-#     )
-#   )
-# }
-#
-#
+x <- replicate(ncol, rnorm(n))
+
+df <- data.frame(x1 = x[,1], x2 = x[,2],
+                 x3 = x[,3])
+
+df$y <- 1 + 2*df$x1 + rnorm(n)
+
+
+prediction_function <- function(theta){
+  return(
+    cbind(1L, df$x1) %*% theta
+  )
+}
+
+# FORMALISM REQUIRED FOR OUR FUNCTIONS
+moment_OLS <- function(theta, ...){
+  return(
+    data.table::data.table(
+      'y' = df$y,
+      'y_hat' = as.numeric(cbind(1L, df$x1) %*% theta),
+      'epsilon' = as.numeric(df$x1*(df$y - cbind(1L, df$x1) %*% theta))
+    )
+  )
+}
+
+
 # objective_function <- function(theta, prediction_function, weights = 1L, return_moment = FALSE, ...){
 #   if (return_moment) return(moment_function(theta))
 #   if (length(weights)==1) return(
@@ -88,8 +108,8 @@ test_that("Method of simulated moments should be close from theoretical paramete
 # #epsilon/as.numeric(cbind(1L, df$x1) %*% theta)
 #   return(t(eps) %*% weights %*% eps)
 # }
-#
-#
+
+
 # # 1: LINEAR REGRESSION ===========
 #
 # ols <- lm(y~ x1, data = df)
@@ -106,9 +126,10 @@ test_that("Method of simulated moments should be close from theoretical paramete
 #
 #
 # msm1 <- estimation_theta(theta_0 = c("const" = 0.1, "beta1" = 0),
-#                          model_function = objective_function,
-#                          prediction_function = moment_function,
-#                          approach = "one_step")
+#                          prediction_function = moment_OLS,
+#                          approach = "two_step")
+
+
 #
 #
 # msm2 <- estimation_theta(theta_0 = c("const" = 0, "beta1" = 0),
